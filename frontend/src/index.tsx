b3ba2056f721e177ecfae0884c8727ca9260b59b
Chalk <donghyeon.hwang@honestfund.kr>
2021-03-17 04:20:04 +0900

    test

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-//react router
-import { BrowserRouter } from "react-router-dom";
+// react router
 
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './components/App'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.querySelector('#root')
-);
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#root')
+)
