e92ec3a72d6dc0ed2e4130e91ee7165719c9d657
DGURI <dguri1997@gmail.com>
2021-04-09 01:44:48 +0900

    월렛 차트 퍼블리싱

--- frontend/src/pages/Wallet.tsx
+++ frontend/src/pages/Wallet.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 import WalletSummary from '@/components/WalletSummary'
 import WalletItem from '@/components/WalletItem'
+import WalletChart from '@/components/WalletChart'
 
 function Wallet() {
-    return (
-        <>
-            <WalletSummary />
-            <WalletItem />
-        </>
-    )
+  return (
+    <>
+      <WalletSummary />
+      <WalletChart />
+      <WalletItem />
+    </>
+  )
 }
 
 export default Wallet
