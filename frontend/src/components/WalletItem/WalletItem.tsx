05fdf2d6b2c7eea98350690211520f41dfc9e1cf
LeeKangbok <pmmm114@gmail.com>
2021-09-15 16:10:14 +0900

    Feat: walletItem 데이터구조, Filp영역추가

--- frontend/src/components/WalletItem/WalletItem.tsx
+++ frontend/src/components/WalletItem/WalletItem.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as S from './styles'
 
 import { WalletItem as TWalletItem } from '@/lib/api/types'
+import Button from '@/components/common/Button'
 
 
 interface WalletItemProps {
@@ -13,53 +14,79 @@ interface WalletItemProps {
 interface IwallItemDataForJSX {
     name: String;
     data: number | String;
+    format?: String | null;
+    unit: String;
 }
 
 function WalletItem({data, valuationAmount}:WalletItemProps) {
+    console.log(data, valuationAmount)
+    const [flipFalg, setFlipFlag] = useState(true);
+
     const contentData = [
         {
             name: '평가손익',
-            data: ((data.ea * data.price) - Math.round(valuationAmount)).toLocaleString(),
+            data: Math.round(valuationAmount) - (data.ea * data.price),
+            format: 'money',
+            unit: '원'
         },
         {
             name: '수익률',
-            data: ((Math.round(valuationAmount) / (data.ea * data.price)) * 100 - 100).toFixed(2) + ' %'
+            data: ((Math.round(valuationAmount) / (data.ea * data.price)) * 100 - 100).toFixed(2),
+            unit: '%'
         }
     ]
     const detailData = [
         {
             name: '매수평균가',
-            data: data.price.toLocaleString()+' 원',
+            data: data.price.toLocaleString(),
+            unit: '원'
         },
         {
             name: '평가금액',
-            data: (valuationAmount ? Math.round(valuationAmount).toLocaleString() : '') + ' 원',
+            data: (valuationAmount ? Math.round(valuationAmount).toLocaleString() : ''),
+            unit: '원'
         },
         {
             name: '매수금액',
-            data: (data.ea * data.price).toLocaleString() + ' 원',
+            data: (data.ea * data.price).toLocaleString(),
+            unit: '원'
         },
         {
             name: '보유수량',
-            data: data.ea + ' ' + data.ticker,
+            data: data.ea,
+            unit: data.ticker
         }
     ]
 
     const contentDataJSX = (contentDataArray: IwallItemDataForJSX[]) => {
         return contentDataArray.map((item, index) => {
-            return <S.contentDescription key={index}>
-                <span>{item.name}</span>
-                <S.testspan color={Number(item.data) > 0 ? 'red' : "#0e52cf"}>{item.data}</S.testspan>
-            </S.contentDescription>
+            return (<React.Fragment key={index}>
+                <S.contentTitle>{item.name}</S.contentTitle>
+                <S.contentDescription color={
+                    Number(item.data) > 0
+                    ? "red"
+                    : (
+                        Number(item.data) < 0 ?"#0e52cf" : undefined
+                    )
+                }>{item.format == 'money' ? item.data.toLocaleString() : item.data} {item.unit}</S.contentDescription>
+            </React.Fragment>)
         })
+    }
+
+    const changeFlipFalg = () => {
+        setFlipFlag(!flipFalg)
     }
 
     const detailDataJSX = (detailDataArray: IwallItemDataForJSX[]) => {
         return detailDataArray.map((item, index) => {
-            return <S.DetailDescription key={index}>
-                <span>{item.name}</span>
-                <span>{item.data}</span>
-            </S.DetailDescription>
+            return (<React.Fragment key={index}>
+                <S.DetailTitle>
+                    {item.name}
+                </S.DetailTitle>
+                <S.DetailDescription>
+                    {item.data} {item.unit}
+                </S.DetailDescription>
+            </React.Fragment>)
         })
     }
 
@@ -73,12 +100,17 @@ function WalletItem({data, valuationAmount}:WalletItemProps) {
                         {data.ticker}
                     </S.TitleTrans>
                 </S.Title>
+                <S.filpTrigger onClick={changeFlipFalg} flipFlag={flipFalg}/>
             </S.Header>
             <S.Content>
                 {contentDataJSX(contentData)}
             </S.Content>
-            <S.Detail>
+            <S.Detail detailFlip={flipFalg} >
                 {detailDataJSX(detailData)}
+                <S.EditButtons>
+                    <Button>삭제</Button>
+                    <Button>수정</Button>
+                </S.EditButtons>
             </S.Detail>
         </S.WalletItem>
     )
--- frontend/src/components/WalletItem/styles.ts
+++ frontend/src/components/WalletItem/styles.ts
@@ -4,16 +4,23 @@ export const WalletItem = styled.div`
     padding: 15px;
     border: 1px solid #333;
     border-radius: 8px;
+    user-select: none;
 
     & + & {
         margin-top: 4px;
     }
 `
 export const Header = styled.div`
+    position: relative;
     font-size: 16px;
 `
-export const Content = styled.div`
-    margin-top: 10px;
+export const Content = styled.dl`
+  display: grid;
+  grid-row-gap: 6px;
+  grid-column-gap: 5px;
+  grid-template-columns: 120px 1fr;
+  margin-top: 15px;
+  font-size: 14px;
 `
 export const Title = styled.div`
     padding-left: 15px;
@@ -27,19 +34,51 @@ export const TitleTrans = styled.span`
     font-size: 12px;
     vertical-align: middle;
 `
-export const contentDescription = styled.div`
-    display: flex;
-    justify-content: space-between;
-    color: #888;
+export const filpTrigger = styled.span`
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: #333;
+    transform: rotate(45deg);
+
+    &::after {
+        position: absolute;
+        top: ${props => props.flipFlag ? -1 : 2}px;
+        left: ${props => props.flipFlag ? -1 : 2}px;
+        display: inline-block;
+        width: 90%;
+        height: 90%;
+        background-color: #fff;
+        transition-timing-function: ease-in-out;
+        transition-duration: 0.1s, 0s;
+        transition-property: top, left;
+        content: '';
+    }
 `
-export const Detail = styled.div`
-    margin-top: 5px
+export const contentTitle = styled.dt`
+  color: #888;
 `
-export const DetailDescription = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-top: 5px
+export const contentDescription = styled.dd`
+  color: ${props => props.color};
+  text-align: right;
 `
-export const testspan = styled.span`
-    color: ${props => props.color}
+export const Detail = styled.dl`
+  display: ${props => props.detailFlip ? 'none' : 'grid'};
+  grid-row-gap: 6px;
+  grid-column-gap: 5px;
+  grid-template-columns: 120px 1fr;
+  margin-top: 15px;
+  font-size: 14px;
+`
+export const DetailTitle = styled.dt`
+  color: #888;
+`
+export const DetailDescription = styled.dd`
+  text-align: right;
+`
+export const EditButtons = styled.div`
+  display: flex
 `
