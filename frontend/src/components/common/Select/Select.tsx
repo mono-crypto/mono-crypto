37b526ec9ef866f9b28374dc3d42f0cabb0053ac
LeeKangbok <pmmm114@gmail.com>
2021-10-27 04:00:28 +0900

    Add: FE - common Select 컴포넌트 생성

---
+++ frontend/src/components/common/Select/Select.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import * as S from './styles'
+
+import { Interpolation } from 'styled-components'
+
+
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+    css?: Interpolation<React.CSSProperties>,
+    options?: string[]
+}
+
+function Select({ css, options, ...rest }: SelectProps) {
+    return(
+        <S.Select {...rest}>
+            <option value="" key={0}>-------</option>
+            {options !== undefined ? options.map((item) => 
+                <option value={item} key={item}>{item}</option>
+            ) : null}
+        </S.Select>
+    )
+}
+
+export default Select
