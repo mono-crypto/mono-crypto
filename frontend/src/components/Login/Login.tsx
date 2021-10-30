371c7f52c9151967e2857b5a063f1b5d9bdee56a
LeeKangbok <pmmm114@gmail.com>
2021-10-31 02:39:40 +0900

    Fix: 구글 gapi 라이브러리 추가

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,34 +1,75 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import * as S from './styles'
 
-// import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import loginHook from '@/hooks/loginHook'
+import Button from '@/components/common/Button';
 
 function Login() {
     console.log('Login component')
-    // const { login, logout, userInfoModal, setUserInfoModal, user } = loginHook();
+    const { login, logout, userInfoModal, setUserInfoModal, user } = loginHook();
 
-    // const userInfoToggle = () => {
-    //     setUserInfoModal(!userInfoModal)
-    // }
+    const userInfoToggle = () => {
+        setUserInfoModal(!userInfoModal)
+    }
 
-    // const responseSuccessGoogle = async(response) => {
-    //     await login(response.tokenId)
-    //     setUserInfoModal(false)
-    // }
+    const loginGoogle = async(user) => {
+        await login(user.getAuthResponse().id_token)
+        setUserInfoModal(false)
+    }
 
     // const responseFailureGoogle = () => {
     //     console.log(res)
     // }
 
-    // const responseLogoutGoogle = () => {
-    //     logout()
-    //     setUserInfoModal(false)
-    // }
+    const logoutGoogle = () => {
+        logout()
+        setUserInfoModal(false)
+    }
+    const loginButton = useRef<HTMLButtonElement>(null)
+
+    useEffect(() => {
+        window.gapi.load('auth2', function() {
+            const googleAuthObj = window.gapi.auth2.init({
+                client_id:
+                    '916373523653-i8f4llc8qb74ktav55p5fc7vmvhgp8n7.apps.googleusercontent.com',
+                cookiepolicy: 'single_host_origin',
+            })
+
+            googleAuthObj.attachClickHandler(loginButton.current, {}, loginGoogle, () => {
+                console.log('login Fail')
+            })
+        });
+    }, [])
 
     return(
         <>
-            LOGIN
+            {user ?
+                <>
+                    <S.CustomButton onClick={userInfoToggle}>
+                        <S.LoginIcon width={'2rem'} height={'2rem'} borderRadius={'2rem'} backgroundImage={user.picture}/>
+                    </S.CustomButton> 
+                    <S.UserInfo display={userInfoModal ? 1 : 0} width={'20rem'}>
+                        <div className="title">
+                            <div className="title_picture">
+                                <img src={user.picture}/>
+                            </div>
+                            <div className="title_info">
+                                <div>{user.name}</div>
+                                <div>{user.email}</div>
+                            </div>
+                        </div>
+                        <div className="menu"></div>
+                        <div className="bottom">
+                            <Button onClick={logoutGoogle}>LOGOUT</Button>
+                        </div>
+                        <button className={'close'} type="button" onClick={userInfoToggle}>close</button>
+                    </S.UserInfo>
+                </>
+                :
+                <S.CustomButton ref={loginButton}>
+                    <S.LoginIcon width={'2rem'} height={'2rem'} borderRadius={'2rem'}/>
+                </S.CustomButton> 
+            }
         </>
     )
 }
